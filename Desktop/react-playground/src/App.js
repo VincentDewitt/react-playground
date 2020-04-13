264a8f4ecbcbf28b32a44f74f1f05cd3bddfd071
Vincent Cataldo <vincentcataldo@me.com>
2020-04-12 21:30:50 -0700

    initial commit

---
+++ Desktop/react-playground/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TheDate from './state/TheDate';
+
+function App() {
+  return (
+    <main className='App'>
+     <TheDate></TheDate>
+    </main>
+  );
+}
+
+export default App;
---
+++ Desktop/react-playground/src/state/TheDate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TheDate extends React.Component {
+    constructor(props) {
+        super(props)
+    
+
+    }
+    render() {
+    return (
+    <div>
+      <TheDate></TheDate>  
+    </div>
+    )
+    }
+}
+export default TheDate 
