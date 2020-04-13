539165d442314ebda362bbe2f8f7ebd38c3f8301
Vincent Cataldo <vincentcataldo@me.com>
2020-04-12 21:40:34 -0700

    intial commit

---
+++ Desktop/trelloyes/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import STORE from './store';
+
+function App() {
+  return (
+    <main className='App'>
+      {console.log(STORE)}
+    </main>
+  );
+}
+
+export default App;
